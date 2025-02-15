a0c68a6ad0397d92138267b33bc669fb6eeb7931
dvdandroid <6277172+DVDAndroid@users.noreply.github.com>
2025-02-15 17:10:57 +0100

    p7m

--- src/main/java/stirling/software/SPDF/controller/api/security/RemoveCertSignController.java
+++ src/main/java/stirling/software/SPDF/controller/api/security/RemoveCertSignController.java
@@ -1,6 +1,12 @@
 package stirling.software.SPDF.controller.api.security;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
@@ -16,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import io.github.pixee.security.BoundedLineReader;
 import io.github.pixee.security.Filenames;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -45,6 +52,40 @@ public class RemoveCertSignController {
             throws Exception {
         MultipartFile pdf = request.getFileInput();
 
+        String originalFilename = pdf.getOriginalFilename();
+        if (originalFilename.endsWith(".pdf.p7m")) {
+            Path tempDir = Files.createTempDirectory("p7m_process");
+            Path tempInputFile = tempDir.resolve("input.pdf.p7m");
+            Files.createDirectories(tempDir);
+            pdf.transferTo(tempInputFile.toFile());
+
+            List<String> command = new ArrayList<>();
+            command.add("/scripts/p7m");
+            command.add(tempInputFile.toString());
+            ProcessBuilder pb = new ProcessBuilder(command);
+            Process process = pb.start();
+            StringBuilder out = new StringBuilder();
+            try (BufferedReader reader =
+                    new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+                String line;
+                while ((line = BoundedLineReader.readLine(reader, 5_000_000)) != null) {
+                    out.append(line);
+                }
+            }
+            int exitCode = process.waitFor();
+            if (exitCode != 0) {
+                throw new RuntimeException("p7m failed with exit code: " + exitCode);
+            }
+            String output = out.toString();
+            String outPdfFilePath = extractFilePath(output);
+            if (outPdfFilePath == null) throw new RuntimeException("p7m errore path");
+            ByteArrayOutputStream document = readFileToByteArrayOutputStream(outPdfFilePath);
+            return WebResponseUtils.boasToWebResponse(
+                    document,
+                    Filenames.toSimpleFileName(originalFilename).replaceFirst("[.][^.]+$", "")
+                            + "_unsigned.pdf");
+        }
+
         // Load the PDF document
         PDDocument document = pdfDocumentFactory.load(pdf);
 
@@ -67,7 +108,29 @@ public class RemoveCertSignController {
         // Return the modified PDF as a response
         return WebResponseUtils.pdfDocToWebResponse(
                 document,
-                Filenames.toSimpleFileName(pdf.getOriginalFilename()).replaceFirst("[.][^.]+$", "")
+                Filenames.toSimpleFileName(originalFilename).replaceFirst("[.][^.]+$", "")
                         + "_unsigned.pdf");
+    }
+
+    private static String extractFilePath(String text) {
+        Pattern pattern = Pattern.compile("'/([^']+)'");
+        Matcher matcher = pattern.matcher(text);
+        if (matcher.find()) {
+            return "/" + matcher.group(1);
+        }
+        return null;
+    }
+
+    public static ByteArrayOutputStream readFileToByteArrayOutputStream(String filePath)
+            throws IOException {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        try (FileInputStream inputStream = new FileInputStream(filePath)) {
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+        }
+        return outputStream;
     }
 }
